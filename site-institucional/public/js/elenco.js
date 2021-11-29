eabdb34e4f348495ead909390328ca598d067f12
LucasOs4kwe <LUCAS.SSANTOS@bandtec.com.br>
2021-11-28 23:02:48 -0300

    Manutenção da página de elenco. O JS ainda precisa ser mexido

---
+++ site-institucional/public/js/elenco.js
@@ -0,0 +1,17 @@
+
+const select = document.getElementById('setor_slct');
+select.addEventListener('change', function(){
+    var valor = setor_slct.value;
+    var ataque = document.getElementsByClassName('atk');
+    var defesa = document.getElementsByClassName('def');
+    var meio = document.getElementsByClassName('mid')
+    var player = document.getElementsByClassName('player-box');
+    if(valor == "ataque"){
+        for(var i = 0; i < player.length; i++){
+            meio[i].style.display = 'none';
+            defesa[i].style.display = 'none';
+        }
+        
+    }
+});
+
