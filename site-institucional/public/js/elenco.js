95d54ce007d29cb6758496155d62ce708c6860d9
LucasOs4kwe <LUCAS.SSANTOS@bandtec.com.br>
2021-12-02 04:16:54 -0300

    JS Final

--- site-institucional/public/js/elenco.js
+++ site-institucional/public/js/elenco.js
@@ -9,6 +9,7 @@ select.addEventListener('change', function(){
                 div[i].style.display = "none";
             }
         }
+        title_elenco.innerHTML = "ATACANTES";
     }else if(valor == "meio_campo"){
         for(var i = 0; i < div.length; i++){
             if(div[i].className == 'player-box atk' || div[i].className == 'player-box def'){
@@ -17,6 +18,7 @@ select.addEventListener('change', function(){
                 div[i].style.display = "block";
             }
         }
+        title_elenco.innerHTML = "MEIO CAMPISTAS";
     }else if(valor == "defesa"){
         for(var i = 0; i < div.length; i++){
             if(div[i].className == 'player-box atk' || div[i].className == 'player-box mid'){
@@ -25,6 +27,7 @@ select.addEventListener('change', function(){
                 div[i].style.display = "block";
             }
         }
+        title_elenco.innerHTML = "DEFENSORES";
     }else{
         for(var i = 0; i < div.length; i++){
             if(div[i].className == 'player-box mid' || div[i].className == 'player-box def' || div[i].className == 'player-box atk'){
@@ -33,6 +36,7 @@ select.addEventListener('change', function(){
                 div[i].style.display = "block";
             }
         }
+        title_elenco.innerHTML = "Elenco Completo";
     }
     
 });
