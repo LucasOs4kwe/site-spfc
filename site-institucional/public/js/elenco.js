45225b5765de9a8472b00ba678d89b415fbf9a45
LucasOs4kwe <LUCAS.SSANTOS@bandtec.com.br>
2021-12-02 04:06:28 -0300

    Página de elenco funcionando com a lógica

--- site-institucional/public/js/elenco.js
+++ site-institucional/public/js/elenco.js
@@ -1,16 +1,39 @@
 
 const select = document.getElementById('setor_slct');
 select.addEventListener('change', function(){
+    var div = document.getElementsByClassName('player-box');
     var valor = setor_slct.value;
-    var ataque = document.getElementsByClassName('atk');
-    var defesa = document.getElementsByClassName('def');
-    var meio = document.getElementsByClassName('mid')
-    var player = document.getElementsByClassName('player-box');
     if(valor == "ataque"){
-        for(var i = 0; i < player.length; i++){
-            meio[i].style.display = 'none';
-            defesa[i].style.display = 'none';
+        for(var i = 0; i < div.length; i++){
+            if(div[i].className == 'player-box mid' || div[i].className == 'player-box def'){
+                div[i].style.display = "none";
+            }
+        }
+    }else if(valor == "meio_campo"){
+        for(var i = 0; i < div.length; i++){
+            if(div[i].className == 'player-box atk' || div[i].className == 'player-box def'){
+                div[i].style.display = "none";
+            }else{
+                div[i].style.display = "block";
+            }
+        }
+    }else if(valor == "defesa"){
+        for(var i = 0; i < div.length; i++){
+            if(div[i].className == 'player-box atk' || div[i].className == 'player-box mid'){
+                div[i].style.display = "none";
+            }else{
+                div[i].style.display = "block";
+            }
+        }
+    }else{
+        for(var i = 0; i < div.length; i++){
+            if(div[i].className == 'player-box mid' || div[i].className == 'player-box def' || div[i].className == 'player-box atk'){
+                div[i].style.display = "block";
+            }else{
+                div[i].style.display = "block";
+            }
         }
     }
+    
 });
 
